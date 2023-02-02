83069e653e3cb6ae292087eff0b1671b90b85dc1
YerayJ <yerayjimeenez07@gmail.com>
2023-02-02 20:17:20 +0100

    Ejercicio 2 bidimensionales

--- src/bidimensionales/Ejercicio2.java
+++ src/bidimensionales/Ejercicio2.java
@@ -5,5 +5,27 @@ public class Ejercicio2 {
         // Creación de tabla con 100 posiciones
         int tabla[][] = new int[10][10];
 
+        // Le introducimos valores
+        // Primero recorre la fila hasta el valor total de la tabla
+        for (int fila = 0; fila < tabla.length; fila++) {
+            // En segundo lugar recorre las columnas de cada fila
+            for (int col = 0; col < tabla[fila].length; col++) {
+                // Rellena la posición con los siguientes valores. Añade uno a los valores para que no empiece en 0
+                tabla[fila][col] = (fila + 1) * (col + 1);
+            }
+        } // Fin de bucle
+
+
+        //Recorre la fila
+        for (int fila = 0; fila < tabla.length; fila++) {
+            // Recorre la columna teniendo en cuenta la longitud de la fila
+            for (int col = 0; col < tabla[fila].length; col++) {
+                // Imprime la tabla mediante tabuladores
+                System.out.print("\t" + tabla[fila][col] + " ");
+            }
+            // Imprime un espacio
+            System.out.println();
+
+        } // Fin de bucle
     }
 }
