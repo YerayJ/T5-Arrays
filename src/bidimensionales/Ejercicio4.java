614852a276932b052cf9a8db69dc98c93eb4acd5
YerayJ <yerayjimeenez07@gmail.com>
2023-02-03 16:26:14 +0100

    Ejercicio 4 bidimensionales terminado

--- src/bidimensionales/Ejercicio4.java
+++ src/bidimensionales/Ejercicio4.java
@@ -5,6 +5,20 @@ public class Ejercicio4 {
         // Creación de notas de 3*6
         int notas[][] = new int[3][6];
 
+        // Primera fila
+        notas[0][1] = 30;
+        notas[0][2] = 2;
+        notas[0][5] = 5;
 
+        // Segunda fila
+        notas[1][0] = 75;
+
+        // Tercera fila
+        notas[2][2] = -2;
+        notas[2][3] = 9;
+        notas[2][5] = 11;
+
+        // Muestra la tabla
+        Funciones.muestraBidimensional(notas);
     }
 }
