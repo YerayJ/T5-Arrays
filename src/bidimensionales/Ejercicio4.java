527ca000a09817824b22e8c3033482043f095c32
YerayJ <yerayjimeenez07@gmail.com>
2023-02-03 12:31:31 +0100

    Ejercicio 4 bidimensionales empezar

---
+++ src/bidimensionales/Ejercicio4.java
@@ -0,0 +1,10 @@
+package bidimensionales;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        // Creación de notas de 3*6
+        int notas[][] = new int[3][6];
+
+
+    }
+}
