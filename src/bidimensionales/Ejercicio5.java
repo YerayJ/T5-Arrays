c5cb35f06e51e345a4e8535e5d565fa204c688b4
YerayJ <you@example.com>
2023-02-06 18:25:07 +0100

    Ejercicio 5 terminado

--- src/bidimensionales/Ejercicio5.java
+++ src/bidimensionales/Ejercicio5.java
@@ -5,14 +5,11 @@ public class Ejercicio5 {
         // Creación de notas de 3*6
         int tabla[][] = new int[4][5];
 
-        // Creación de variable auxiliar suma de las filas
-        int sumaFila = 0;
-
         // Creación variable suma de filas
         int total = 0;
 
-        // Creación de variable auxiliar suma de las filas
-        int sumaCol = 0;
+        // Creación de variable auxiliar suma
+        int suma = 0;
 
         // Le introducimos valores
         // Primero recorre la fila hasta el valor total de la tabla
@@ -25,29 +22,32 @@ public class Ejercicio5 {
         } // Fin de bucle
 
         for (int fila = 0; fila < tabla.length; fila++) {
-            sumaFila = 0;
+            suma = 0;
             for (int col = 0; col < tabla[fila].length; col++) {
                 // Añadimos a la suma el valor de la tabla
-                sumaFila += tabla[fila][col];
+                suma += tabla[fila][col];
                 // Añadimos todos los valores al total
                 total += tabla[fila][col];
                 // Imprime linea por línea
-                System.out.print("\t" + tabla[fila][col]);
+                System.out.print("\t" + " " + tabla[fila][col]);
             }
-            System.out.print(" " + sumaFila);
+            // Por último imprime la suma de cada fila al final
+            System.out.print(" " + suma);
             System.out.println();
         }
-
-        for (int col = 0; col < tabla.length; col++) {
-            sumaCol = 0;
-            for (int fila = 0; fila < tabla[col].length; fila++) {
+        // Inicia un bucle que serán las columnas hasta la longitud de la posición 0, que en este caso es 4
+        for (int col = 0; col < tabla[0].length; col++) {
+            // Inicializa la variable suma a 0
+            suma = 0;
+            // Comprueba todas las filas
+            for (int fila = 0; fila < tabla.length; fila++) {
                 // Añadimos a la suma el valor de la tabla
-                sumaCol += tabla[col][fila];
-
+                suma += tabla[fila][col];
             }
-            System.out.print("\t" + sumaCol);
+            // Imprime la suma
+            System.out.print("\t" + suma);
         }
-
-
+        // Imprime el total de todo
+        System.out.print(" " + total);
     }
 }
